1380b22d04e0cc5d13bf193ea612facce2b8de93
Mick Morrow <mmorrow24work@gmail.com>
2026-04-14 17:52:54 +0100

    Update HelloJobsGit.py

--- jobs/HelloJobsGit.py
+++ jobs/HelloJobsGit.py
@@ -3,10 +3,10 @@ from nautobot.apps.jobs import Job, register_jobs
 class HelloJobsGit(Job):
 
     class Meta: 
-        name = "Hello Jobs from Git Repo"
+        name = "Hello Jobs from Git Repo - HelloJobsGit.py"
 
     def run(self):
-        self.logger.debug("This is from the Git repo.")
+        self.logger.debug("This is from the Git repo - HelloJobsGit.py")
 
 
 register_jobs(
