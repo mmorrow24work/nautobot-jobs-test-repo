69d6dfe3cd15a4e6d681c7cfa34a5520220b00a7
Mick Morrow <mmorrow24work@gmail.com>
2026-04-15 10:13:31 +0100

    Create file_upload.py

---
+++ jobs/file_upload.py
@@ -0,0 +1,18 @@
+from nautobot.apps.jobs import Job, register_jobs, FileVar
+
+class FileUpload(Job):
+    class Meta:
+        name = "CSV File Upload"
+        description = "Please select a CSV file for upload"
+
+    file = FileVar(
+        description="CSV File to upload",
+    )
+
+    def run(self, file):
+        
+        contents = str(file.read())
+        self.logger.info(f"File contents: {contents}")
+        self.logger.info(f"Job didn't crash!")
+
+        return "Great job!"
