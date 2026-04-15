cd3ec543c7ef25292a4839410744ef8075df774a
Mick Morrow <mmorrow24work@gmail.com>
2026-04-15 10:19:33 +0100

    Update file_upload.py

--- jobs/file_upload.py
+++ jobs/file_upload.py
@@ -1,5 +1,7 @@
 from nautobot.apps.jobs import Job, register_jobs, FileVar
 
+name = "Jobs Collection nautobot-jobs-test-repo"
+
 class FileUpload(Job):
     class Meta:
         name = "CSV File Upload"
