6054029e19adcb70b8618b513a927c3b05a2ef02
Mick Morrow <mmorrow24work@gmail.com>
2026-04-15 10:14:29 +0100

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,5 +1,6 @@
 from nautobot.apps.jobs import register_jobs
 from .hello_jobs import HelloJobs
 from .HelloJobsGit import HelloJobsGit
+from .HelloJobsGit import file_upload
 
-register_jobs(HelloJobs, HelloJobsGit)
+register_jobs(HelloJobs, HelloJobsGit, FileUpload)
